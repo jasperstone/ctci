5edab6bd5e45b6d5bee25a835f08944ba6321ea1
advancedbasic <advancedbasic@gatech.edu>
2017-02-20 23:54:15 -0600

    Getting set up to start coding

---
+++ Chapter01/Q01_1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ctci.Contracts;
+
+namespace Chapter01
+{
+    public class Q01_1 : IQuestion
+    {
+        public void Run()
+        {
+            Console.WriteLine("This is chapter 1 question 1.");
+        }
+    }
+}
